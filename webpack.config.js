43d67995ce8c4a6bcaa9c6b9c3d0d11cdc9675ea
Ali Bahaari <alibahaari1997@gmail.com>
2021-09-25 00:03:03 +0330

    Build Phase

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.bundle.js'
+    },
+    mode: 'development', // Options: 'production', 'development' and 'none'
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+}
